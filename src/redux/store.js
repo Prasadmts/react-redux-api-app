21bd693a6b727bb9708fa1045ed8bdf427acb963
Prasadmts <prasadmts2507@gmail.com>
2023-07-04 20:02:13 +0530

    added functional code

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from "redux"; 
+import postReducer from "./reducers/postReducer"; 
+import thunk from "redux-thunk";
+
+
+// import { combineReducers } from "redux";
+
+// const rootReducer = combineReducers({
+//     post: postReducer
+// })
+
+
+const store = createStore(postReducer, applyMiddleware(thunk));
+
+export default store;
